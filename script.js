bfede3f594e4c2265b33ffee6df9628d9f4b1d85
benfarrelly752 <44237240+benfarrelly752@users.noreply.github.com>
2025-05-29 16:55:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,139 +1,133 @@
-var currentRoom = "start";
-var currentConversation = "scobes";
-var commands = ["go", "pickup", "inventory", "talk", "examine", "pickup", "use"];
-var inventory = ["baccy", "snickers"];
+// Refactored Global State
+const gameState = {
+    currentRoom: "start",
+    currentConversation: "scobes",
+    commands: ["go", "pickup", "inventory", "talk", "examine", "use"],
+    inventory: ["baccy", "snickers"]
+};
 
+// Helper function to append text to game-text
+const appendToGameText = (text) => {
+    $('#game-text').append(`<p>${text}</p>`);
+};
+
+const updateBackground = (room) => {
+    const description = `Image of ${room} in an immersive text adventure game.`;
+    fetch(`/generate-image?description=${encodeURIComponent(description)}`)
+    .then(response => response.json())
+    .then(data => {
+        $('body').css('background-image', `url(${data.imageUrl})`);
+    })
+    .catch(error => console.error('Error fetching image:', error));
+};
+
+// Command functions leveraging new gameState and helper function
 function pickup(item) {
-    if (rooms[currentRoom].pickup[item] !== undefined) {
-        currentConversation = rooms[currentRoom].pickup[item]
-        $('#game-text').append("<p>" + rooms[currentRoom].pickup[item] + "</p>");
-        inventory.push(item); 
+    if (rooms[gameState.currentRoom].pickup && rooms[gameState.currentRoom].pickup[item] !== undefined) {
+        gameState.currentConversation = rooms[gameState.currentRoom].pickup[item];
+        appendToGameText(rooms[gameState.currentRoom].pickup[item]);
+        gameState.inventory.push(item);
     } else {
-        $('#game-text').append("<p>Don't be lookin at that ya bastard!!</p>");
+        appendToGameText("Don't be lookin at that ya bastard!!");
     }
 }
 
 function use(item) {
-    const found = false;
-    for (var i = 0; i < inventory.length; i++) {
-    if (inventory[i] == item ) {
-        found = true;
-        $('#game-text').append("<p>You used the glock</p>");
-        if(rooms[currentRoom].use[item] !== undefined){
-        currentConversation = rooms[currentRoom].use[item]
-        $('#game-text').append("<p>" + rooms[currentRoom].use[item] + "</p>");
+    const found = gameState.inventory.includes(item); // Simplified with Array.includes
+    if (found) {
+        appendToGameText(`You used the ${item}`);
+        if (rooms[gameState.currentRoom].use && rooms[gameState.currentRoom].use[item] !== undefined) {
+            gameState.currentConversation = rooms[gameState.currentRoom].use[item];
+            appendToGameText(rooms[gameState.currentRoom].use[item]);
         }
+    } else {
+        appendToGameText("You don't have this item you spud");
     }
 }
-    if(found == false){
-        $('#game-text').append("<p>You don't have this item you spud</p>");
-
-    }
-
-}
-     
 
 function showExamine(item) {
-    if (rooms[currentRoom].examine[item] !== undefined) {
-        currentConversation = rooms[currentRoom].examine[item]
-        $('#game-text').append("<p>" + rooms[currentRoom].examine[item] + "</p>");
+    if (rooms[gameState.currentRoom].examine && rooms[gameState.currentRoom].examine[item] !== undefined) {
+        gameState.currentConversation = rooms[gameState.currentRoom].examine[item];
+        appendToGameText(rooms[gameState.currentRoom].examine[item]);
     } else {
-        $('#game-text').append("<p>Don't be examining that ya bastard!!</p>");
+        appendToGameText("Don't be examining that ya bastard!!");
     }
-
-
 }
 
 function talkto(npc) {
-    if (rooms[currentRoom].speak[npc] !== undefined) {
-        currentConversation = rooms[currentRoom].speak[npc]
-        $('#game-text').append("<p>" + rooms[currentRoom].speak[npc] + "</p>");
+    if (rooms[gameState.currentRoom].speak && rooms[gameState.currentRoom].speak[npc] !== undefined) {
+        gameState.currentConversation = rooms[gameState.currentRoom].speak[npc];
+        appendToGameText(rooms[gameState.currentRoom].speak[npc]);
     } else {
-        $('#game-text').append("<p>You cannot talk to them!!</p>");
+        appendToGameText("You cannot talk to them!!");
     }
-
-
 }
 
 function changeRoom(dir) {
-    if (rooms[currentRoom].directions[dir] !== undefined) {
-        currentRoom = rooms[currentRoom].directions[dir]
-        $('#game-text').append("<p>" + rooms[currentRoom].description + "</p>");
+    if (rooms[gameState.currentRoom].directions && rooms[gameState.currentRoom].directions[dir] !== undefined) {
+        gameState.currentRoom = rooms[gameState.currentRoom].directions[dir];
+        appendToGameText(rooms[gameState.currentRoom].description);
+        updateBackground(gameState.currentRoom); // Update background on room change
     } else {
-        $('#game-text').append("<p>You cannot do that</p>");
+        appendToGameText("You cannot go that way.");
     }
-
-
 }
 
-
 function showHelp() {
-    $('#game-text').append("<p>Here are the possible commands: </p>");
-    $('#game-text').append("<p><ul>");
-    for (var i = 0; i < commands.length; i++) {
-        $('#game-text').append("<li>" + commands[i] + "</li>");
-    }
-    $('#game-text').append("</ul></p>");
-
+    appendToGameText("Here are the possible commands: ");
+    gameState.commands.forEach(command => appendToGameText(`<li>${command}</li>`));
 }
 
 function showInventory() {
-    if (inventory.length === 0) {
-        $('#game-text').append("<p>You are not carrying anything!</p>");
+    if (gameState.inventory.length === 0) {
+        appendToGameText("You are not carrying anything!");
         return;
     }
-    $('#game-text').append("<p>Here is your inventory: </p>");
-    $('#game-text').append("<p><ul>");
-    for (var i = 0; i < inventory.length; i++) {
-        $('#game-text').append("<li>" + inventory[i] + "</li>");
-    }
-    $('#game-text').append("</ul></p>");
-
+    appendToGameText("Here is your inventory:");
+    gameState.inventory.forEach(item => appendToGameText(`<li>${item}</li>`));
 }
 
 function playerInput(input) {
-    var command = input.split(" ")[0];
+    const command = input.split(" ")[0];
+    const arg = input.split(" ")[1];
     switch (command) {
         case "go":
-            var dir = input.split(" ")[1];
-            changeRoom(dir);
+            changeRoom(arg);
             break;
         case "talk":
-            var npc = input.split(" ")[1];
-            talkto(npc);
+            talkto(arg);
             break;
         case "help":
             showHelp();
             break;
-            case "examine":
-             var item = input.split(" ")[1];
-            showExamine(item);
+        case "examine":
+            showExamine(arg);
             break;
-             case "pickup":
-             var item = input.split(" ")[1];
-            pickup(item);
+        case "pickup":
+            pickup(arg);
             break;
-             case "use":
-             var item = input.split(" ")[1];
-            use(item);
+        case "use":
+            use(arg);
             break;
         case "inventory":
             showInventory();
             break;
         default:
-            $('#game-text').append("<p>Invalid command!</p>");
+            appendToGameText("Invalid command!");
     }
 }
- 
+
 $(document).ready(function() {
-    $('#game-text').append("<p>" + rooms.start.description + "</p>");
+    // Initial message about how to use commands
+    appendToGameText("Use commands like 'go', 'pickup', 'talk', 'examine', 'use', and 'inventory' to interact with the world.");
+    appendToGameText(rooms.start.description);
+    updateBackground('start'); // Initialize background on game start
+
     $(document).keypress(function(key) {
         if (key.which === 13 && $('#user-input').is(':focus')) {
-            var value = $('#user-input').val().toLowerCase();
+            const value = $('#user-input').val().toLowerCase();
             $('#user-input').val("");
             playerInput(value);
         }
-    })
-
-
-})
+    });
+});
